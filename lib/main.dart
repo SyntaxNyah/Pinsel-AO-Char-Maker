badfa7e60d22f9467dd7df37434d0299fb734a74
SyntaxNyah <235356625+SyntaxNyah@users.noreply.github.com>
2026-06-04 12:58:10 +0100

    build fix?
    build fix?

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:flutter/foundation.dart'; // FlutterExceptionHandler (not re-exported by material)
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
